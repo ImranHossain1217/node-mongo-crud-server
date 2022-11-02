a3c0c6c261d502908a5b6b339b4e4f0666312d35
Imran Hossain <imranhossainemon1717@gmail.com>
2022-11-03 01:07:00 +0600

    node-mongo-crud-server

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// userId: userdb1
+// userPassword: q8fp3jfcy3FXIuEc
+
+const uri =
+  "mongodb+srv://userdb1:q8fp3jfcy3FXIuEc@cluster0.vwrnpfj.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("NodeCrud").collection("users");
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const user = req.body;
+      const option = { upsert: true };
+      const updateUser = {
+        $set: {
+          name: user.name,
+          country: user.country,
+          email: user.email,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateUser, option);
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Node mongodb CRUd server runnig !!");
+});
+
+app.listen(port, () => {
+  console.log(`Node mongo server port is ${port}`);
+});
